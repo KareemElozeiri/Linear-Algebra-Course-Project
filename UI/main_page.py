92ca5a0592364cc670384d9c1232a308dfb67f43
kareem elozeiri <k.elozeiri@gmail.com>
2022-02-01 00:54:07 +0200

    more improvements in the structure of the project for modularity

--- UI/main_page.py
+++ UI/main_page.py
@@ -32,39 +32,22 @@ class MainPage(GridLayout):
         self.status_bar.height = 30
         self.add_widget(self.status_bar)
     
-    #swtiches the screen manager of our app to the processing page 
-    def switch_to_processing(self,_):
-        self.app.screen_manager.current = "processing page"
+   
 
     #removes the content of the status bar of the main page
     def empty_status_bar(self,*_):
         self.status_bar.text = ""
-
-    '''
-    the function creates the processing page if already not created 
-    if it is created it just updates the main scene of the processing page by the new image loaded/captured by the user
-    '''
-    def create_processing_page(self,img):
-        if(self.app.processing_page == None):
-            self.app.processing_page = ProcessingPage(self.app,img)
-            screen = Screen(name="processing page")
-            screen.add_widget(self.app.processing_page)
-            self.app.screen_manager.add_widget(screen)
-        else:
-            self.app.processing_page.img = img 
-            self.app.processing_page.convert_to_texture()
-            self.app.processing_page.update_main_scene()
           
     #function for loading image that is going to be processed 
     def load_img(self):
-        self.create_processing_page(cv2.imread("C:\\Kareem El-ozeiri\\Linear-Algebra-Course-Project\\UI\\img.jpg"))
+        self.app.create_processing_page(cv2.imread("C:\\Kareem El-ozeiri\\Linear-Algebra-Course-Project\\UI\\img.jpg"))
         self.status_bar.text = "loading an image"      
         Clock.schedule_once(self.empty_status_bar,1)
-        Clock.schedule_once(self.switch_to_processing,1)
+        Clock.schedule_once(self.app.switch_to_processing,1)
 
     #function for capturing the image that is going to be processed by the web cam of the computer 
     def capture_img(self):
-        self.create_processing_page(cv2.imread("C:\\Kareem El-ozeiri\\Linear-Algebra-Course-Project\\UI\\img.jpg"))
+        self.app.create_processing_page(cv2.imread("C:\\Kareem El-ozeiri\\Linear-Algebra-Course-Project\\UI\\img.jpg"))
         self.status_bar.text = "capturing an image"      
         Clock.schedule_once(self.empty_status_bar,1) 
-        Clock.schedule_once(self.switch_to_processing,1)
+        Clock.schedule_once(self.app.switch_to_processing,1)
--- app.py
+++ app.py
@@ -23,6 +23,25 @@ class MyApp(App):
         self.screen_manager.add_widget(screen)
 
         return self.screen_manager
+    
+    '''
+    the function creates the processing page if already not created 
+    if it is created it just updates the main scene of the processing page by the new image loaded/captured by the user
+    '''
+    def create_processing_page(self,img):
+        if(self.processing_page == None):
+            self.processing_page = ProcessingPage(self,img)
+            screen = Screen(name="processing page")
+            screen.add_widget(self.processing_page)
+            self.screen_manager.add_widget(screen)
+        else:
+            self.processing_page.img = img 
+            self.processing_page.convert_to_texture()
+            self.processing_page.update_main_scene()
+    
+    #swtiches the screen manager of our app to the processing page 
+    def switch_to_processing(self,_):
+        self.screen_manager.current = "processing page"
 
 if __name__ == "__main__":
     app = MyApp()
