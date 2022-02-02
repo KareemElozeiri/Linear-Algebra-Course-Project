da019a8e5b1e503542b25e4b27f6d02eb27c5569
kareem elozeiri <k.elozeiri@gmail.com>
2022-02-02 17:17:38 +0200

    small fix within the median blur script

--- Effects/median_blur.py
+++ Effects/median_blur.py
@@ -14,12 +14,19 @@ def set_kernel_values(img,row_initial,col_initial,kernel_size,value):
    img[row_initial+kernel_size//2,col_initial+kernel_size//2] = value 
 
 def median_blur(img, kernel_size):
-    blurred_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    for row in range(0,len(blurred_img)-kernel_size):
-        for col in range(0,len(blurred_img[0])-kernel_size):
-            median = get_kernel_median(blurred_img,row,col,kernel_size)
-            set_kernel_values(blurred_img,row,col,kernel_size,median)
+    gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    final_img = gray_img
+    for row in range(0,len(gray_img)-kernel_size):
+        for col in range(0,len(gray_img[0])-kernel_size):
+            median = get_kernel_median(gray_img,row,col,kernel_size)
+            set_kernel_values(final_img,row,col,kernel_size,median)
     
-    blurred_img = cv2.cvtColor(blurred_img,cv2.COLOR_GRAY2RGB)
+    blurred_img = cv2.cvtColor(final_img,cv2.COLOR_GRAY2BGR)
     return blurred_img
 
+if __name__ == "__main__":
+        img = cv2.imread("Ggstokes.jpg")
+        blurred = median_blur(img,5)
+        cv2.imshow("org",img)
+        cv2.imshow("blur",blurred)
+        cv2.waitKey(0)
